91dacca0d312daafa5d822c90babf76f43a04196
yuisil-dev <yuisil.dev@gmail.com>
2025-05-26 21:21:05 +0900

    Remove event_category from GA event tracking

--- js/analytics.js
+++ js/analytics.js
@@ -2,4 +2,13 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-1CLC4VX76Q'); 
+gtag('config', 'G-1CLC4VX76Q');
+
+// Common event tracking function
+function trackUIClick(label, target = null, elementType) {
+  gtag('event', 'ui_click', {
+    'event_label': label,
+    'target': target,
+    'element_type': elementType
+  });
+} 
